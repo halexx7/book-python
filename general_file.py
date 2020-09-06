e4e74eac45d172d9aec05bd434c8295b184e09cd
Aleksey Khlybov <halexx7@gmail.com>
2020-09-06 11:15:37 +0300

    Add odd2.py, mod general_file.py

--- general_file.py
+++ general_file.py
@@ -12,7 +12,52 @@
 # # вывод даты в формате гггг-мм-дд
 # print(datetime.date.isoformat(datetime.date.today()))
 
-import time
-print(time.strftime("%I:%M"))
-print(time.strftime("%A %p"))
+# import time
+# print(time.strftime("%I:%M"))
+# print(time.strftime("%A %p"))
 
+# from datetime import datetime
+#
+# odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
+#         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+#         41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+#
+# right_this_minute = datetime.today().minute
+#
+# if right_this_minute in odds:
+#     print('This minute seems a little odd.')
+# else:
+#     print('Not an odd minute.')
+
+# for i in [1, 2, 3]:
+#     print(i)
+
+# for ch in 'Hi!':
+#     print(ch)
+
+# # Получаем инфорамцию о функции
+# import random
+# help(random.randint)
+
+# # Случайное число в диапазоне
+# import random
+# print(random.randint(1, 60))
+
+from datetime import datetime
+import time
+import random
+
+odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
+        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+        41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+
+    if right_this_minute in odds:
+        print('This minute seems a little odd.')
+    else:
+        print('Not an odd minute.')
+    wait_time = random.randint(1, 10)
+    time.sleep(wait_time)
+    
