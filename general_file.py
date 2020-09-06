b4c98d02f08e697696e3dfcfa72803243abdf499
Aleksey Khlybov <halexx7@gmail.com>
2020-09-06 08:41:28 +0300

    Add general_file.py

---
+++ general_file.py
@@ -0,0 +1,18 @@
+# import sys
+# print(sys.version)
+
+# import os
+# os.getcwd()
+
+# import datetime
+# print(datetime.date.today().day)
+# print(datetime.date.today().month)
+# print(datetime.date.today().year)
+#
+# # вывод даты в формате гггг-мм-дд
+# print(datetime.date.isoformat(datetime.date.today()))
+
+import time
+print(time.strftime("%I:%M"))
+print(time.strftime("%A %p"))
+
